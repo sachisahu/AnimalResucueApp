5e7f85f9ac7f08caf957e26c7fd31ee8973d09fb
Sachidanand Sahu <1901227390@cgu-odisha.ac.in>
2023-12-04 17:13:04 +0530

    url updates for register animal

--- AnimalRes/views.py
+++ AnimalRes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
     return render(request, "home.html")
+
+
+def registerAnimal(request):
+    return render(request, "NewAnimal.html")
