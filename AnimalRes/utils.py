371456749d7dfbecc89fed7fdfc06cad44c4163a
Sachidanand Sahu <1901227390@cgu-odisha.ac.in>
2023-12-07 10:59:21 +0530

    digitalocean url updates

--- AnimalRes/utils.py
+++ AnimalRes/utils.py
@@ -9,7 +9,7 @@ from  AnimalResucueApp import settings
 
 def uploadResourcesToDigitalOcean(folderName, file):
     storage = S3Boto3Storage()
-    image_path = f"resources/{str(folderName)}/{str(generate_random_alphanumeric(5))}{file.name}"
+    image_path = f"resources/{str(folderName)}/{str(generate_random_alphanumeric(40))}{file.name}"
     storage.save(image_path, file)
     fileUrl = "https://animalrescuesresources.blr1.digitaloceanspaces.com/resources/" + str(image_path)
     return fileUrl
